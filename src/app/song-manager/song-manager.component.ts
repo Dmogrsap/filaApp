dc4b08f83fb2dd136cd73141290bdee0a9020188
Dmogrsap <72819804+Dmogrsap@users.noreply.github.com>
2025-08-21 23:15:37 -0600

    cambios en letras y acordes

--- src/app/song-manager/song-manager.component.ts
+++ src/app/song-manager/song-manager.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SongManagerService } from '../services/song-manager.service';
 import { DataType } from 'devextreme/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-song-manager',
@@ -25,10 +26,10 @@ export class SongManagerComponent implements OnInit {
   ngOnInit() {
     this.songService.getSongs().subscribe((result) => {
       this.datasourceSongs = result.sort((a, b) =>
-        a.Nombre.localeCompare(b.Nombre)
+        a.Titulo.localeCompare(b.Titulo)
       );
       this.loadIndicatorVisible = false;
-      //console.log('DataSource', this.dataSourceMenus);
+      console.log('datasourceSongs', this.datasourceSongs);
     });
   }
 
@@ -65,33 +66,51 @@ export class SongManagerComponent implements OnInit {
     // e.cancel = true;
   }
 
+  getDisplayExpr(item: any) {
+    if (!item) {
+      return '';
+    }
+    return `$ ${item.Nombre}, `;
+  }
+
+  onSelectionChanged(
+    selectedRowKeys: any,
+    cellInfo: any,
+    dropDownBoxComponent: any
+  ) {
+    cellInfo.value = selectedRowKeys[0];
+    if (selectedRowKeys.length > 0) {
+      dropDownBoxComponent.close();
+    }
+  }
+
   onSaving(e: any) {
-    // const change = e.changes[0];
-    // if (change) {
-    //   e.cancel = false;
-    // }
-    // if (change.type == 'insert') {
-    //   // Limpia los campos no válidos
-    //   const cleanData = { ...change.data };
-    //   Object.keys(cleanData).forEach((key) => {
-    //     if (/^__.*__$/.test(key)) {
-    //       delete cleanData[key];
-    //     }
-    //   });
-    //   this.userService.addUser(cleanData).then((docRef) => {
-    //     //console.log('Usuario agregado con ID:', docRef.id);
-    //     Swal.fire({
-    //       icon: 'success',
-    //       title: 'success',
-    //       text: 'User Added Successfully!',
-    //     });
-    //     this.userService.getUsers().subscribe((result) => {
-    //       this.dataSourceUsers = result.sort((a, b) =>
-    //         a.Nombre.localeCompare(b.Nombre)
-    //       );
-    //     });
-    //   });
-    // }
+    const change = e.changes[0];
+    if (change) {
+      e.cancel = false;
+    }
+    if (change.type == 'insert') {
+      // Limpia los campos no válidos
+      const cleanData = { ...change.data };
+      Object.keys(cleanData).forEach((key) => {
+        if (/^__.*__$/.test(key)) {
+          delete cleanData[key];
+        }
+      });
+      this.songService.addSongs(cleanData).then((docRef) => {
+        //console.log('Usuario agregado con ID:', docRef.id);
+        Swal.fire({
+          icon: 'success',
+          title: 'success',
+          text: 'User Added Successfully!',
+        });
+        this.songService.getSongs().subscribe((result) => {
+          this.datasourceSongs = result.sort((a, b) =>
+            a.Nombre.localeCompare(b.Nombre)
+          );
+        });
+      });
+    }
     // if (change.type == 'update') {
     //   // Limpia los campos no válidos
     //   const cleanData = { ...change.data };
@@ -140,7 +159,7 @@ export class SongManagerComponent implements OnInit {
 
   onRowClick(e: any) {
     this.selectedSong = e.data;
-    this.previewText = `Título: ${e.data.Titulo}\nAcordes: ${e.data.Acordes}\nLetra:\n${e.data.Letra}`;
+    this.previewText = `Título: ${e.data.Titulo}\n \nAcordes: \n\n ${e.data.Acordes}\n \nLetra: \n\n ${e.data.Letra}`;
   }
 
   onSelect(e: any) {
@@ -152,5 +171,15 @@ export class SongManagerComponent implements OnInit {
     return this.showOnlySunday
       ? this.datasourceSongs.filter((song) => song.isActive)
       : this.datasourceSongs;
+  }
+
+  get previewTitulo() {
+    return this.selectedSong ? this.selectedSong.Titulo : '';
+  }
+  get previewAcordes() {
+    return this.selectedSong ? this.selectedSong.Acordes : '';
+  }
+  get previewLetra() {
+    return this.selectedSong ? this.selectedSong.Letra : '';
   }
 }
