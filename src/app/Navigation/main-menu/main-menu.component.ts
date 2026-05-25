191d51970143326bb1f87f250fc13e2befbf689c
Dmogrsap <72819804+Dmogrsap@users.noreply.github.com>
2026-05-24 21:55:39 -0600

    casi funcional pedidos cafe

--- src/app/Navigation/main-menu/main-menu.component.ts
+++ src/app/Navigation/main-menu/main-menu.component.ts
@@ -33,7 +33,7 @@ const ROUTE_ROLES: { [key: string]: string[] } = {
   '/servidor-maestros': ['Admin', 'Maestra', 'Maestro', 'Lider Maestras', 'Lider Maestros'],
   '/calendario-materiales': ['Admin', 'Maestra', 'Maestro', 'Lider Maestras', 'Lider Maestros'],
   '/change-pass': [''], // Ruta pública, sin roles requeridos
-  '/filastorecliente': [''], // Ruta pública, sin roles requeridos
+  '/filastorecliente': ['Admin','Lider Fila Store', 'Cliente'], // Ruta pública, sin roles requeridos
 };
 
 @Component({
