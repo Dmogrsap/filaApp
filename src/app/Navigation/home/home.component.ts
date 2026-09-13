1cb58b8b9b718061d031ee0c3a323d30fd715fcf
Dmogrsap <72819804+Dmogrsap@users.noreply.github.com>
2026-09-13 12:41:16 -0600

    transmision en vivo

--- src/app/Navigation/home/home.component.ts
+++ src/app/Navigation/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { FirebaseStorageService } from 'src/app/services/image.service';
 import Swal from 'sweetalert2';
 
@@ -10,12 +11,22 @@ import Swal from 'sweetalert2';
 export class HomeComponent implements OnInit {
 
   public datasoureimages: any | null = null;
-  public itemsGaleria: string[] = [
-    'assets/img/filaIntro1.jpg',
-    'assets/img/filaIntro.jpg'
+  public hayTransmisionEnVivo = true;
+  public facebookLiveUrl: SafeResourceUrl;
+  public facebookLiveVideoLink = 'https://www.facebook.com/Iglesiafiladelfiach/videos/28160375423612066';
+  public itemsGaleria: Array<{ type: 'image'; url: string }> = [
+    { type: 'image', url: 'assets/img/filaIntro1.jpg' },
+    { type: 'image', url: 'assets/img/filaIntro.jpg' }
   ];
 
-  constructor(private FirebaseStorageService: FirebaseStorageService) { }
+  constructor(
+    private FirebaseStorageService: FirebaseStorageService,
+    private sanitizer: DomSanitizer
+  ) {
+    this.facebookLiveUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      `https://www.facebook.com/plugins/video.php?href=${encodeURIComponent(this.facebookLiveVideoLink)}&show_text=false&autoplay=false&width=560`
+    );
+  }
 
   ngOnInit(): void {
     Swal.fire({
